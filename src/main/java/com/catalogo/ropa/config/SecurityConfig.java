32930a6b4669edeb2536cd9d9fc311e14bb252df
mariana0812rc-cmd <mariana.0812@gmail.com>
2026-07-26 13:15:29 -0600

    Fix: agregar import de List en SecurityConfige

--- src/main/java/com/catalogo/ropa/config/SecurityConfig.java
+++ src/main/java/com/catalogo/ropa/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
